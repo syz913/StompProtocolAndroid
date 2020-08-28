6a1f088fc62530a333cbdb78db88909f2f80977b
syz913 <2518846395@qq.com>
2020-08-29 00:24:41 +0800

    add

--- lib/src/main/java/ua/naiksoftware/stomp/pathmatcher/SubscriptionPathMatcher.java
+++ lib/src/main/java/ua/naiksoftware/stomp/pathmatcher/SubscriptionPathMatcher.java
@@ -2,15 +2,17 @@ package ua.naiksoftware.stomp.pathmatcher;
 
 import ua.naiksoftware.stomp.dto.StompHeader;
 import ua.naiksoftware.stomp.dto.StompMessage;
+import ua.naiksoftware.stomp.StompClient;
 
 public class SubscriptionPathMatcher implements PathMatcher {
 
     @Override
     public boolean matches(String path, StompMessage msg) {
        // Compare subscription
-        String pathSubscription = stompClient.getTopicId(path);
-        if (pathSubscription == null) return false;
-        String subscription = msg.findHeader(StompHeader.SUBSCRIPTION);
-        return pathSubscription.equals(subscription);
+//        String pathSubscription = stompClient.getTopicId(path);
+//        if (pathSubscription == null) return false;
+//        String subscription = msg.findHeader(StompHeader.SUBSCRIPTION);
+//        return pathSubscription.equals(subscription);
+        return  true;
     }
 }
