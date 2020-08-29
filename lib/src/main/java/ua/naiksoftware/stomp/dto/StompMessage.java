6bd6e057fc642fda917fc572a1727390e1a26e8d
syz913 <2518846395@qq.com>
2020-08-29 11:52:23 +0800

    add

--- lib/src/main/java/ua/naiksoftware/stomp/dto/StompMessage.java
+++ lib/src/main/java/ua/naiksoftware/stomp/dto/StompMessage.java
@@ -85,7 +85,11 @@ public class StompMessage {
             matcher.find();
             headers.add(new StompHeader(matcher.group(1), matcher.group(2)));
         }
-    
+        try {
+            reader.skip("\n\n");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         reader.useDelimiter(TERMINATE_MESSAGE_SYMBOL);
         String payload = reader.hasNext() ? reader.next() : null;
 
